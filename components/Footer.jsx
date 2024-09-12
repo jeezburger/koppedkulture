79261bb57ada5be0e01489abaa22cad1b0c9a707
veer aryan <43904888+jeezburger@users.noreply.github.com>
2024-09-13 01:45:17 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillInstagram, AiFillFacebook } from 'react-icons/ai';
 import Link from 'next/link';
+
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2024 Kopped Kulture All rights reserverd</p>
+      <p>2024 Kopped Kulture All rights reserved</p>
       <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a href="https://www.instagram.com/koppedkulture" target="_blank" rel="noopener noreferrer">
+          <AiFillInstagram />
+        </a>
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          <AiFillFacebook />
+        </a>
       </p>
       <p>
-        <Link href="/policy">Contact Us & Shipping-Return Policy </Link>
-        <Link href="https://www.termsfeed.com/live/f078e3ea-c8bb-4662-b253-d5722ce3ef60">Privacy Policy </Link>
-        <Link href="/tandc">T&C </Link>
+        <Link href="/policy">Contact Us & Shipping-Return Policy</Link>
+        <Link href="https://www.termsfeed.com/live/f078e3ea-c8bb-4662-b253-d5722ce3ef60">Privacy Policy</Link>
+        <Link href="/tandc">T&C</Link>
       </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
