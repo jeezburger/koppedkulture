03b18d0b5525b173dd29268dc0e868274c0487d4
anvay-acs <anvayarora010@gmail.com>
2024-09-12 14:02:23 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <AiOutlineTwitter />
       </p>
       <p>
-        <Link href="/policy">Shipping Policy </Link>
+        <Link href="/policy">Contact Us & Shipping-Return Policy </Link>
         <Link href="https://www.termsfeed.com/live/f078e3ea-c8bb-4662-b253-d5722ce3ef60">Privacy Policy </Link>
         <Link href="/tandc">T&C </Link>
       </p>
