d9cdf19cb0e3179dfe0c234b78384cf40e435b7b
anvay-acs <anvayarora010@gmail.com>
2024-09-14 15:08:50 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
-                    rel="stylesheet"
+                    rel="stylesheet" 
                 />
             </Head>
             <body>
